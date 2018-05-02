015ca41dc5c59cbddf460e6fd84990f891bba8ba
Alumno DAT <alumno@dat14.(none)>
2018-05-02 14:08:34 +0200

    comprobación-horas-4

--- WEB-INF/classes/cancelar_reserva.java
+++ WEB-INF/classes/cancelar_reserva.java
@@ -3,9 +3,6 @@ import java.util.*;
 import java.awt.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
 import activities.db.*;
 
 public class cancelar_reserva extends HttpServlet {
@@ -19,25 +16,6 @@ public class cancelar_reserva extends HttpServlet {
 
  
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
-		String mail = null;
-
-		HttpSession session1 = req.getSession(true);
-		String NIAKey = new String("NIA");
-        String userID = (String)session1.getAttribute(NIAKey);
-		 
-		 final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-         Properties props = System.getProperties();
-		 props.setProperty("mail.smtp.host", "smtp.gmail.com");
-		 props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-		 props.setProperty("mail.smtp.socketFactory.fallback", "false");
-		 props.setProperty("mail.smtp.port", "465");
-		 props.setProperty("mail.smtp.socketFactory.port", "465");
-		 props.put("mail.smtp.auth", "true");
-		 props.put("mail.debug", "true");
-		 props.put("mail.store.protocol", "pop3");
-		 props.put("mail.transport.protocol", "smtp");
-		 final String username = "reservas.box2box@gmail.com";//
-		 final String password = "gentefuerte1+";
 		
       String numres = req.getParameter("cancelar");
        try{
